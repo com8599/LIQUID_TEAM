696b645632fe4a4e9deb7062f7e43804bfd9d3df
com8599 <com8599@gmail.com>
2019-06-12 20:34:25 +0900

    sub_name add
    지하철 이름만 모은 api를 넣었습니다

---
+++ TeamProject/app/src/main/java/com/example/team/SubwayName.kt
@@ -0,0 +1,5 @@
+package com.example.team
+
+data class SubwayName(var line_num:String, var station_cd:String, var station_nm: String, var fr_code:String) {
+    //line_num:호선, station_cd:전철역코드, station_nm:전철역 이름, fr_code:외부코드
+}
