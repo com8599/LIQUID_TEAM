742e1139e9799aaaa559195aeac3d13bb99daf46
enfys <enfyswon07@gmail.com>
2019-06-26 23:16:40 +0900

    route, guide

--- TeamProject/app/src/main/java/com/example/team/GuideActivity.kt
+++ TeamProject/app/src/main/java/com/example/team/GuideActivity.kt
@@ -1,8 +1,10 @@
 package com.example.team
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.MediaController
 import kotlinx.android.synthetic.main.activity_guide.*
 
 class GuideActivity : AppCompatActivity() {
@@ -10,6 +12,12 @@ class GuideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guide)
+
+        val video :Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.konkuk)
+        video_guide.setMediaController(MediaController(this));
+        video_guide.setVideoURI(video)
+
+        video_guide.start();
 
         leftroute_guide.setOnClickListener {
             val routeIntent = Intent(this, RouteResultActivity::class.java)
